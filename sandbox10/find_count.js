83e2f8c185f2ce9523af3605553b2ba989dfb8cc
Yuuma-ujimoto <yxy.yuuma@gmail.com>
2021-03-30 01:22:28 +0900

    sandbox10作成 mongoDBの応用

---
+++ sandbox10/find_count.js
@@ -0,0 +1,31 @@
+const mongodb = require("mongodb")
+const MongoClient = mongodb.MongoClient
+
+const option = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+MongoClient.connect("mongodb://localhost:27017/mydb",
+    option,
+    (error, client) => {
+        if(error){
+            throw error
+        }
+        check_mail_address(client,"test@example.com",(result)=>{
+            console.log(result)
+            client.close()
+        })
+    })
+
+
+const check_mail_address = (db,mail_address,callback)=>{
+    const dbname = db.db("mydb")
+    dbname.collection("user").countDocuments({mail_address:mail_address},(error, documents) => {
+        if (error) {
+            throw error
+        }
+//        console.log(documents)
+        callback(documents)
+    })
+}
---
+++ sandbox10/readme.md
@@ -0,0 +1,13 @@
+## mongoDB AP
+mongoDBの応用
+
+### find_count.js
+mysqlで
+
+```sql
+select count(*) from ~~~~
+```
+こう書いてた処理をMongoバージョンで書いた感じ
+
+count関数でもできるがcountDocuments関数の方が推奨されているらしい.
+
