88132cc425b632d3beab467a7f948c16ec7b8239
Yuuma-ujimoto <yxy.yuuma@gmail.com>
2021-04-17 19:32:56 +0900

    update box12 sign-upとsign-inのファイル名が逆になっていたのを修正

--- box12-Sign-In-And-Sign-Up-MongoDB-template/sign-in.js
+++ box12-Sign-In-And-Sign-Up-MongoDB-template/sign-in.js
@@ -6,49 +6,38 @@ const config = require("./mongodb-config")
 
 
 router.get("/",(req, res) => {
-    res.render("sign-in",{error:false})
+    res.render("sign-up",{error:false})
 })
 
 router.post("/",async (req, res, next) => {
     const mail_address = req.body.mail_address
-    const user_name = req.body.user_name
     const password = res.body.password
-
-    // メールアドレス正規表現チェック
-    const mail_pattern = "/^[\w\-._]+@[\w\-._]+\.[A-Za-z]+$/"
-    if(mail_address.match(mail_pattern)){
-        res.render("sign-up",{error:true})
-        return
-    }
-
-    const post_data_check =  !mail_address||!password||!user_name
+    const post_data_check =  !mail_address||!password
     if(post_data_check){
-        res.render("sign-up",{error:true})
+        res.render("sign-in",{error:true})
         return
     }
     let client
     try {
         client = await MongoClient.connect(config.url,config.option)
-        const db = client.db("mydb")
+        const db = client.db(config.dbname)
         const user_collection = db.collection("user")
-        const check_mail_address = await user_collection.countDocuments({mail_address:mail_address})
-        if(check_mail_address){
+        const check_user = await user_collection.countDocuments({mail_address:mail_address,password:password})
+        if(!check_user){
+            // パスワードとメールアドレスの組み合わせが一致しない場合
             res.render("sign-in",{error:true})
             return
         }
-        await user_collection.insertOne({user_name:user_name,mail_address:mail_address,password:password})
-        const user_data = await user_collection.findOne({mail_address:mail_address})
-        req.session.user_id = user_data._id
-        res.redirect("/")
     }
-
     catch (e) {
         console.log(e)
         res.render("sign-in",{error:true})
+        return
     }
     finally {
-       await client.close()
+        await client.close()
     }
+    res.redirect("/")
 })
 
 module.exports = router
