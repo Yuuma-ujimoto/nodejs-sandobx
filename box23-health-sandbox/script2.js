0e9cf639f638d8b9f79ad47902a40c96caffcb25
Yuuma-ujimoto <yxy.yuuma@gmail.com>
2021-07-23 02:07:25 +0900

    update

--- box23-health-sandbox/script2.js
+++ box23-health-sandbox/script2.js
@@ -12,9 +12,10 @@ console.log(def_list)
 let def_list2 = ["a"]
 
 let def_list3 = def_list2.concat(["b","c"])
-
-console.log(def_list3)
+def_list2 = def_list3
+console.log(def_list2)
 
 // 実行結果[ 'a', 'b', 'c' ]
 // 欲しい結果にはなった
 // concatはpushと違って指定した配列を操作するのではなく連結した配列を戻り値で返す仕様になってるので注意
+
